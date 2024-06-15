261e5d3c6f5e3da71c60f3728ba1165728b12886
fastube <120088732+fastube@users.noreply.github.com>
2024-06-15 08:16:23 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const $versi = "v0";
+const $versi = "v1";
 const $caches = [
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
   "https://fonts.googleapis.com/css2?family=Freeman&family=Jost:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
   "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.min.js",
-  "./bell.svg","./chevron.svg","./dl-white.svg","./download.svg","./info.svg","./lightbulb-white.svg","./addlist.svg","./lightbulb.svg","./remove.svg","./reply.svg","./xbell.svg","./favicon.png","./youtube.png"
+  "./bell.svg","./chevron.svg","./dl-white.svg","./download.svg","./info.svg","./lightbulb-white.svg","./addlist.svg","./lightbulb.svg","./remove.svg","./reply.svg","./xbell.svg","./favicon.png","./youtube.png","./ig.png"
 ];
 
 self.addEventListener('install', e => {
