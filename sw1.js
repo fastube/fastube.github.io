8f659f597469052f5bbab9a98041384c03f17d39
fastube <120088732+fastube@users.noreply.github.com>
2024-06-25 18:20:31 +0700

    Add files via upload

--- sw1.js
+++ sw1.js
@@ -1,4 +1,4 @@
-const $versi = "0.0.1";
+const $versi = "0.0.1a";
 const $caches = [
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
   "https://fonts.googleapis.com/css2?family=Freeman&family=Jost:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
@@ -6,6 +6,7 @@ const $caches = [
   "https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css",
   "//acscdn.com/script/aclib.js",
+  "https://www.youtube.com/player_api",
   "./bell.svg","./chevron.svg","./dl-white.svg","./download.svg","./info.svg","./lightbulb-white.svg","./addlist.svg","./lightbulb.svg","./remove.svg","./reply.svg","./xbell.svg","./favicon.png","./youtube.png","./ig.png"
 ];
 
